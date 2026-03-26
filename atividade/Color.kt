6f8758baff8773ddd6ff6e1ad4aedeacf395cbbe
Samuel Veríssimo <svas@discente.ifpe.edu.br>
2026-03-26 10:18:55 -0300

    Add files via upload

---
+++ atividade/Color.kt
@@ -0,0 +1,11 @@
+package com.example.composenavigationapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val ColorA1 = Color(0xFFFFFFFF)
+val ColorA2 = Color(0xFFFFFFFF)
+val ColorA3 = Color(0xFFFFFFFF)
+
+val ColorB1 = Color(0xFF4CAF50)
+val ColorB2 = Color(0xFF305631)
+val ColorB3 = Color(0xFF003801)
